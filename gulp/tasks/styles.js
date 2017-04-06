a8bd5e76403fc34510ca843d1a63df38a7acd1df
Louis <louis.sc@gmail.com>
2017-04-06 15:13:29 -0400

    files updated

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
     var plugins = [
         cssImport,
+        mixins,
         cssvars,
         nested,
         autoprefixer
@@ -15,5 +17,9 @@ gulp.task('styles', function(){
     //include return here because gulp.src is an asynchronous function
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss(plugins))
+        .on('error', function(errorInfo){
+            console.log(errorInfo.toString());
+            this.emit('end');  
+        })
         .pipe(gulp.dest('./app/temp/styles'));
 });
